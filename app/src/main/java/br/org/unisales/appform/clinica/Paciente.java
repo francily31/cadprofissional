b638c15dcef1ac2ca0fc32a3837e7bae88c8dac9
HumbertoGO1227 <humbertoliveira28@gmail.com>
2023-11-09 21:10:35 -0300

    Inicio paciente

--- app/src/main/java/br/org/unisales/appform/clinica/Paciente.java
+++ app/src/main/java/br/org/unisales/appform/clinica/Paciente.java
@@ -1,4 +1,45 @@
 package br.org.unisales.appform.clinica;
 
+
+import org.dizitart.no2.IndexType;
+import org.dizitart.no2.NitriteId;
+import org.dizitart.no2.objects.Id;
+import org.dizitart.no2.objects.Index;
+import org.dizitart.no2.objects.Indices;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Indices(value = {
+        @Index(value = "cpf", type = IndexType.Unique)
+})
+
 public class Paciente {
+@Id
+    public NitriteId id;
+    public String nome;
+
+    public Date dtnascimento;
+
+    public Number telefone;
+
+    public String email;
+
+    @Override
+
+    public String toString(){
+
+        return this.nome;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Paciente paciente = (Paciente) o;
+        return Objects.equals(id, paciente.id);
+    }
+
+
+
+
 }
--- app/src/main/java/br/org/unisales/appform/persistencia/BaseDados.java
+++ app/src/main/java/br/org/unisales/appform/persistencia/BaseDados.java
@@ -2,11 +2,16 @@ package br.org.unisales.appform.persistencia;
 
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
+
+import br.org.unisales.appform.clinica.Paciente;
 import br.org.unisales.appform.clinica.Profissional;
 
 public class BaseDados {
     //repositorio de objetos para a entidade profissional
     public static ObjectRepository<Profissional> rProfissional;
+
+    public static ObjectRepository<Paciente> rPaciente;
+
 
     public static void init (String file) {
         //instancia do banco de dados
@@ -16,5 +21,8 @@ public class BaseDados {
                 .openOrCreate("root", "salesiano");
 
         rProfissional = db.getRepository(Profissional.class);
+        rPaciente = db.getRepository(Paciente.class);
+
+
     }
 }
